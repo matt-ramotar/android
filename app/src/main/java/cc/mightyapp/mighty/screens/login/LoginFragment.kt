669ab6b94dc15d698511ce4390be5b4feae84068
Matt Ramotar <matt.ramotar@jhu.edu>
2021-05-08 14:45:22 -0400

    Configure

---
+++ app/src/main/java/cc/mightyapp/mighty/screens/login/LoginFragment.kt
@@ -0,0 +1,27 @@
+package cc.mightyapp.mighty.screens.login
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import cc.mightyapp.mighty.R
+import cc.mightyapp.mighty.databinding.LoginFragmentBinding
+
+class LoginFragment : Fragment() {
+    private lateinit var binding: LoginFragmentBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(
+            inflater, R.layout.login_fragment, container, false
+        )
+
+        return binding.root
+    }
+
+}
