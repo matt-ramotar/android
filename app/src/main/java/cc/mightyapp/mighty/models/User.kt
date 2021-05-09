33d697c42ef63d04fd538a18d07646b72a4ae7af
Matt Ramotar <matt.ramotar@jhu.edu>
2021-05-09 07:59:16 -0400

    Add User data class

---
+++ app/src/main/java/cc/mightyapp/mighty/models/User.kt
@@ -0,0 +1,21 @@
+package cc.mightyapp.mighty.models
+
+
+data class User(
+    val id: String,
+    val firstName: String,
+    val lastName: String,
+    val email: String,
+    val isLoggedIn: Boolean,
+    val workouts: List<Any>,
+    val xp: Int,
+    val poundsLiftedTotal: Int,
+    val level: String,
+    val achievements: List<Any>,
+    val usersFollowing: List<Any>,
+    val usersFollowedBy: List<Any>,
+    val routinesFavorited: List<Any>,
+    val routinesAuthored: List<Any>,
+    val programsFollowing: List<Any>,
+    val programsAuthored: List<Any>
+)
